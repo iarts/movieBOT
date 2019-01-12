946ae10e382aeec432be3e546172ef6b773e7abe
richter4 <richterbaggos@hotmail.com>
2019-01-12 16:47:39 +0200

    s

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ output += `
 <h5>${movie.Title}</h5>
 <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Λεπτομέρειες ταινίας</a>
 </br></br>
-<p>Παρακαλώ Επιλέψτε Για Την Αποθήκευση Της Ταινίας</p>
+<p>Παρακαλώ Επιλέξτε Για Την Αποθήκευση Της Ταινίας</p>
 Επιλογή: <input type="checkbox" class="get_value" name="movie" value="${movie.imdbID}">
 </br>
 Αξιολόγηση : <x-star-rating value="3" number="5"></x-star-rating>
